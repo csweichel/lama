0b03e5c72b04048ad15378c3706ca8037189a4e0
Christian Weichel <chris@gitpod.io>
2022-05-02 20:33:20 +0000

    Build for arm64

--- build/pack-with-upx.sh
+++ build/pack-with-upx.sh
@@ -4,4 +4,4 @@ if [ "$GITPOD_WORKSPACE_ID" != "" ]; then
     export PATH=/workspace:$PATH
 fi
 
-find dist -type f -name lama | grep linux | xargs upx --brute $i
+find dist -type f -name lama | grep linux | grep -v arm64 | xargs upx --brute
